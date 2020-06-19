282a393e2e541d974b54e95f03d374f417b57eae
wafeelka <49671882+wafeelka@users.noreply.github.com>
2020-06-19 16:31:10 +0300

    Upload files: DataBase.cs; MainWindow.xaml.cs

---
+++ DataBase.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_App_for_students.Class
+{
+    class DataBase
+    {
+        MySqlConnection connection = new MySqlConnection("server = localhost; port=3306; " +
+                                                         "username = root; password = root; database =authentificationdata");
+        public void openConnect()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed) { connection.Open(); }
+        }
+        public void closedConnect()
+        {
+            if (connection.State == System.Data.ConnectionState.Open) { connection.Close(); }
+        }
+        public MySqlConnection GetConnection() { return connection; }
+    }
+}
+ 
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using MySqlConnector;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using The_App_for_students.Class;
+using MySql.Data.MySqlClient;
+
+namespace The_App_for_students
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Sing_In_Click(object sender, RoutedEventArgs e)
+        {
+            string login = loginField.Text;
+            string password = passField.Text;
+            DataBase db = new DataBase();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT* FROM `users` WHERE `Login` = @ul AND `Password` = @up", db.GetConnection());
+            command.Parameters.Add("@ul", MySqlDbType.VarChar).Value = login;
+            command.Parameters.Add("@up", MySqlDbType.VarChar).Value = password;
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            if(table.Rows.Count > 0) {MessageBox.Show("Authentification succeed");}
+            else { MessageBox.Show("Authentification failed"); }
+        }
+    }
+}
+ 
